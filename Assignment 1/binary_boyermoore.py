eed0d7fd39461ba691edfb60dd98ae7638bd665c
Dax246 <damien.ambegoda@gmail.com>
2021-10-11 15:54:47 +1100

    Committing files for Assignment 1, 2 and 3

--- Assignment 1/binary_boyermoore.py
+++ Assignment 1/binary_boyermoore.py
@@ -1,12 +1,170 @@
 
-"""
-Get rid of bad character rule. Good suffix has a number of situations where it ensures it does not pull a suffix that
-mismatches for the same reason. Because this is binary, when it ensures it does not mismatch the same reason, it actually
-pulls out the next character that matches with the mismatch (if it matches 0, it will get a 1). This does what
-bad character rule does so there's no need for BC. Need to account for the edge case where there are no 1s or 0s but
-maybe good suffix already accounts for it
+""" Description of method used to solve question 2, binary_boyermoore, of the assignment.
+The standard boyermoore algorithm was used however galil's optimisation was optimised even more for binary. This
+algorithm subtracts one from galil_start from the standard algorithm (except when using a matched prefix). This
+is a safe skip because normal good suffix ensures a matching substring is shifted forward but it also ensures that
+the previous substring does not mismatch for the same reason. For example if 011 was already matched and it mismatches
+on a 1, the good suffix method will find a previous substring that matches 011 as well as not being preceded by a 1.
+In non binary situations, the character preceding must be checked as good suffix just ensures it is not the same
+character that was originally mismatched. In this binary situation, when it ensures it does not mismatch for the same
+reason, it actually also finds the matching character as the length of the alphabet is only 2.
+
+Bad Character was also removed from the algorithm as good suffix does the job of bad character in binary alphabets. The
+explanation above both finds a matching substring as well as finding a character that matches the mismatched character.
+This was removed from the algorithm but it does not save comparisons.
 
 """
+import sys
 
-# Bad character rule probably doesn't help much considering that there are so many occurrences of each letter in the
-# alphabet.
+# Modified from algorithm presented in FIT3155 lectures
+def binary_boyermoore(txt, pat):
+
+    # Finds the z values and z suffixes
+    reverse_pat_list = []
+    for i in range(len(pat) - 1, -1, -1):
+        reverse_pat_list.append(pat[i])
+    reverse_pat = "".join(reverse_pat_list)
+    z_suffix_reverse = z_calculator(reverse_pat)
+    z_suffix = []
+    for i in range(len(z_suffix_reverse)-1, -1, -1):
+        z_suffix.append(z_suffix_reverse[i])
+    z_values = z_calculator(pat)
+
+    # Finds good suffixes
+    # -1 is used for when there is no good suffix. 0 is not used because 0 is an index
+    good_suffix = [-1 for _ in range(len(pat)+1)]
+    for i in range(len(pat)-1):
+        j = len(pat) - z_suffix[i]
+        good_suffix[j] = i
+
+    # Matched prefixes are also preprocessed
+    matched_prefix = [0 for _ in range(len(pat)+1)]
+    curr_max_mp = z_values[len(pat)-1]
+    for i in range(len(z_values)-1, -1, -1):
+        if z_values[i] > curr_max_mp:
+            curr_max_mp = z_values[i]
+        matched_prefix[i] = curr_max_mp
+    matched_prefix[0] = len(pat)
+
+    res = []
+    galil_start = -10
+    galil_stop = -10
+    comparison_count = 0
+    curr_index = len(pat)-1
+    while curr_index < len(txt):
+        mismatch_check = False
+        i = 0
+        k = len(pat) - 1 - i
+        while i < len(pat):
+
+            # Applies galil skip
+            galil_skip = 0
+            if len(pat) - 1 - i == galil_stop:
+                while len(pat) - 1 - (i + galil_skip) >= galil_start:
+                    galil_skip += 1
+                i += galil_skip
+            if i == len(pat):
+                continue
+
+            # If a mismatch, breaks out of loop otherwise iterates i if there is a match
+            comparison_count += 1
+            if pat[len(pat)-1 - i] != txt[curr_index - i]:
+                mismatch_check = True
+                mismatch_char = txt[curr_index - i]
+                break
+            i += 1
+
+        # If there has been a mismatch
+        if mismatch_check:
+
+            # good suffix
+            # Case 1a
+            if good_suffix[len(pat) - i] > -1:
+                good_suffix_shift = len(pat) - good_suffix[len(pat) - i] - 1
+
+                # Galil_start is -1 from the usual algorithm which is the optimisaiton
+                galil_start = good_suffix[len(pat) - i] - (len(pat)-1) + (len(pat) - 1 - i) + 1 - 1
+                galil_stop = good_suffix[len(pat) - i]
+
+
+            # Case 1b using matched prefix
+            else:
+                good_suffix_shift = len(pat) - matched_prefix[len(pat) - i]
+                galil_start = 0
+                galil_stop = matched_prefix[len(pat) - i] - 1
+
+        # Case 2 when pat matches txt
+        elif not mismatch_check:
+            res.append(curr_index - i + 1)
+            good_suffix_shift = len(pat) - matched_prefix[1]
+            galil_start = 0
+            galil_stop = matched_prefix[1] - 1
+
+        # Accounts for edge case when prefix matches all of suffix (so avoids matched prefix) but with the galil
+        # optimisation, galil_start is -1 which is not an index
+        if galil_start < 0:
+            galil_start = 0
+
+        # Applies the skip
+        curr_index += good_suffix_shift
+
+    # Saves start of matching txt index to output_binary_boyermoore
+    f = open("output_binary_boyermoore.txt", "w")
+    for match in res:
+        f.write(str(match) + "\n")
+    f.close()
+    # Prints all comparison counts to terminal
+    print(comparison_count)
+    return
+
+# Z algorithm based on the FIT3155 Lectures
+def z_calculator(zString):
+    z_values = [0 for _ in range(len(zString))]
+    z_values[1] = naive_comparison(zString, 1, 0)
+    if z_values[1] > 0:
+        r = z_values[1]
+        l = 1
+    else:
+        r = 0
+        l = 0
+    for k in range(2, len(zString)):
+        if k > r:
+            z_values[k] = naive_comparison(zString, k, 0)
+            if z_values[k] > 0:
+                r = z_values[k] + k - 1
+                l = k
+        elif k <= r:
+            if z_values[k - l] < (r - k + 1):
+                z_values[k] = z_values[k - l]
+            elif z_values[k - l] >= r - k + 1:
+                q = naive_comparison(zString, r + 1, r - k + 1)
+                z_values[k] = q
+                r = q + k - 1
+                l = k
+    return z_values
+
+def naive_comparison(zString, zString_index, pat_index):
+    while zString[pat_index] != "$" and zString_index < len(zString):
+        if zString[pat_index] != zString[zString_index]:
+            break
+        else:
+            pat_index += 1
+            zString_index += 1
+    return pat_index
+
+# Algorithm based on FIT3155 Lecture
+def readFile(txtFilename, patFilename):
+    patFile = open(patFileName, "r")
+    pat = patFile.read()
+    patFile.close()
+    txtFile = open(txtFileName, "r")
+    txt = txtFile.read()
+    txtFile.close()
+    return txt, pat
+
+# Following based on FIT3155 Lecture
+if __name__ == "__main__":
+    txtFileName = sys.argv[1]
+    patFileName = sys.argv[2]
+    txt, pat = readFile(txtFileName, patFileName)
+    binary_boyermoore(txt, pat)
--- Assignment 1/editdist.py
+++ Assignment 1/editdist.py
@@ -1,32 +1,115 @@
 
 
+""" Description of method used to solve question 1, editdist, of the assignment.
+
+The standard Gusfield's Z algorithm was used to determine Z values and Z suffixes. This was used in a standard pattern
+matching algorithm using Z values. The difference is that normal Z values are used to determine how much of the pat
+matches the txt. If the whole pattern does not match, the algorithm attempts to insert, substitute and delete the
+mismatch part of the txt. Then the algorithm will use the Z suffixes to determine if the rest of the pattern matches
+the relevant part of the txt.
+
+Calculating Z values and Z suffixes is O(m+n) time in the preprocessing stage where m is len(pat) and n is len(txt).
+Then the algorithm will run through  each part of the txt and do O(1) work for each character which is overall O(n)
+work.
+
 """
-Calculate Z i values and Z suffix values. If len(z box) = len(pattern) then there is a match. For each index
-where z box is not long enough, check the character after the z box (if z box is empty it will be that character) and
-apply all three edits (insert, delete, substitution). Then use the good suffixes to check the rest of the string to
-see if it matches
-"""
+from copy import deepcopy
+import sys
 
 def editdist(txt, pat):
+
+    # Z Value preprocessing
     zString = pat + "$" + txt
     z_values = z_calculator(zString)
+
+    # Z suffix value preprocessing
     zSuffixString = txt + "$" + pat
     reverse_string_list = [zSuffixString[i] for i in range(len(zSuffixString)-1, -1, -1)]
     reverse_string = "".join(reverse_string_list)
-    z_suffix = z_calculator(reverse_string)
-    print("z_suffix", z_suffix)
-    print("z_values", z_values)
+
+    # z_suffix_in_wrong_order puts the z value at the end of the matched substring. We want at front of the matched
+    # substring
+    z_suffix_in_wrong_order = z_calculator(reverse_string)
+    z_suffix = []
+    for i in range(len(pat)-1, -1, -1):
+        z_suffix.append(z_suffix_in_wrong_order[i])
+    z_suffix.append(0)
+    index = len(zString) - 1
+    for i in range(len(txt)):
+        z_suffix.append(z_suffix_in_wrong_order[index])
+        index -= 1
+
+    z_suffix_correct = deepcopy(z_suffix)
+    for i in range(len(z_suffix)):
+        x = z_suffix[i]
+        if x > 0:
+            z_suffix_correct[i-x+1] = x
+            if x != 1:
+                z_suffix_correct[i] = 0
+
+
     res = []
     for i in range(len(pat)+1, len(zString)):
+
+        # If the pat matches the txt perfectly
         if z_values[i] == len(pat):
             matchingtxt = []
             for j in range(len(pat)):
                 matchingtxt.append(zString[i+j])
-            res.append([i, matchingtxt])
+            res.append([i - len(pat) - 1, 0])
         else:
 
-    return res
 
+            mismatch_txt_index = i + z_values[i]
+
+            # insert when at end of txt
+            if mismatch_txt_index == len(zString):
+                if z_values[i] == len(pat) - 1:
+                    matchingtxt = []
+                    for j in range(len(pat) - 1):
+                        matchingtxt.append(zString[i + j])
+                    res.append([i - len(pat) - 1, 1])
+
+            # insert edit
+            elif z_suffix_correct[mismatch_txt_index] == len(pat) - z_values[i] - 1:
+                matchingtxt = []
+                for j in range(len(pat) - 1):
+                    matchingtxt.append(zString[i + j])
+                res.append([i - len(pat) - 1, 1])
+
+            # delete edit
+            elif z_suffix_correct[mismatch_txt_index] == len(pat) - z_values[i]:
+                matchingtxt = []
+                for j in range(len(pat) - 1):
+                    matchingtxt.append(zString[i + j])
+                res.append([i - len(pat) - 1, 1])
+
+            # substitution edit
+
+            # This is if substitution occurs at the end of the txt
+            elif mismatch_txt_index == len(zString):
+                if z_values[i] == len(pat) - 1:
+                    matchingtxt = []
+                    for j in range(len(pat)):
+                        matchingtxt.append(zString[i + j])
+                    res.append([i - len(pat) - 1, 1])
+
+            # Normal substitution
+            elif mismatch_txt_index + 1 < len(zString):
+                if z_suffix_correct[mismatch_txt_index+1] == len(pat) - z_values[i] - 1:
+                    matchingtxt = []
+                    for j in range(len(pat) - 1):
+                        matchingtxt.append(zString[i + j])
+                    res.append([i - len(pat) - 1, 1])
+
+    # Save results to output_editdist.txt
+    f = open("output_editdist.txt", "w")
+    for match in res:
+        f.write(str(match[0]) + " " + str(match[1]) + "\n")
+    f.close()
+    return
+
+# Z algorithm based on the FIT3155 Lectures
 def z_calculator(zString):
     z_values = [0 for _ in range(len(zString))]
     z_values[1] = naive_comparison(zString, 1, 0)
@@ -39,13 +122,16 @@ def z_calculator(zString):
     for k in range(2, len(zString)):
         if k > r:
             z_values[k] = naive_comparison(zString, k, 0)
+            if z_values[k] > 0:
+                r = z_values[k] + k - 1
+                l = k
         elif k <= r:
-            if z_values[k - l + 1] < (r - k + 1):
-                z_values[k] = z_values[k - l + 1]
-            elif z_values[k - l + 1] >= r - k + 1:
-                q = naive_comparison(zString, r + 1, r - k + 2)
-                z_values[k] = q - k
-                r = q - 1
+            if z_values[k - l] < (r - k + 1):
+                z_values[k] = z_values[k - l]
+            elif z_values[k - l] >= r - k + 1:
+                q = naive_comparison(zString, r + 1, r - k + 1)
+                z_values[k] = q
+                r = q + k - 1
                 l = k
     return z_values
 
@@ -58,6 +144,19 @@ def naive_comparison(zString, zString_index, pat_index):
             zString_index += 1
     return pat_index
 
+# Algorithm based on FIT3155 Lecture
+def readFile(txtFilename, patFilename):
+    patFile = open(patFileName, "r")
+    pat = patFile.read()
+    patFile.close()
+    txtFile = open(txtFileName, "r")
+    txt = txtFile.read()
+    txtFile.close()
+    return pat, txt
 
-#editdist("acdef", "abcdef")
-print(editdist("aabcdef", "abcd"))
+# Following based on FIT3155 Lecture
+if __name__ == "__main__":
+    txtFileName = sys.argv[1]
+    patFileName = sys.argv[2]
+    pat, txt = readFile(txtFileName, patFileName)
+    editdist(txt, pat)
